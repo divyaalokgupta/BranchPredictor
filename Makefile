3152cc522a8795d547368efa24aad0fb16553434
divyaalokgupta <taruntanuj007@gmail.com>
2015-11-12 20:50:02 -0500

    GShare Branch Predicton is working.

--- Makefile
+++ Makefile
@@ -11,6 +11,15 @@ bimodal:
 	gvimdiff 3 ./ValidationRuns/val_bimodal_3.txt
 	gvimdiff 4 ./ValidationRuns/val_bimodal_4.txt
 
+gshare:
+	./sim gshare 9 3 traces/gcc_trace.txt > 1
+	./sim gshare 14 8 traces/gcc_trace.txt > 2
+	./sim gshare 11 5 traces/jpeg_trace.txt > 3
+	./sim gshare 10 6 traces/perl_trace.txt > 4
+	gvimdiff 1 ./ValidationRuns/val_gshare_1.txt
+	gvimdiff 2 ./ValidationRuns/val_gshare_2.txt
+	gvimdiff 3 ./ValidationRuns/val_gshare_3.txt
+	gvimdiff 4 ./ValidationRuns/val_gshare_4.txt
 
 clean:
 	rm -f branchpredictor.o
--- branchpredictor.c
+++ branchpredictor.c
@@ -20,16 +20,10 @@ int main(int argc, char *argv[])
                 exit(-1);
         }
 
-        FILE *trace = fopen(argv[3],"r");
-        if(trace == NULL)
-        {
-                printf("FATAL: Could not open argv[3]\n");
-                exit(-1);
-        }
 
         unsigned short bimodal_prediction_table[262143];
         unsigned short gshare_prediction_table[262143];
-        unsigned long long global_branch_history;
+        unsigned long long bhr;
         unsigned short hybrid_chooser_table[262143];
         char str[12];
 
@@ -38,6 +32,12 @@ int main(int argc, char *argv[])
         
         if(!strcmp(argv[1],"bimodal"))
         {
+            FILE *trace = fopen(argv[3],"r");
+            if(trace == NULL)
+            {
+                    printf("FATAL: Could not open argv[3]\n");
+                    exit(-1);
+            }
             unsigned long long i, num_addr, branch_addr, address_bits = atoi(argv[2]);
             char t;
             
@@ -110,13 +110,107 @@ int main(int argc, char *argv[])
             
             for(i=0;i<pow(2,address_bits);i++)
                 printf(" %d  %d\n",i,bimodal_prediction_table[i]);
+            fclose(trace);
         }
         else if (!strcmp(argv[1],"gshare"))
         {
+            FILE *trace = fopen(argv[4],"r");
+            if(trace == NULL)
+            {
+                    printf("FATAL: Could not open argv[4]\n");
+                    exit(-1);
+            }
+            unsigned long long i, num_addr, branch_addr, address_bits = atoi(argv[2]), bhr_bits = atoi(argv[3]);
+            bhr = 0;
+            char t;
+            
+            //Initialize prediction counters
+            for(i=0;i<pow(2,address_bits);i++)
+                gshare_prediction_table[i] = 2;
+
+            while(fgets(str,12,trace) != NULL)
+            {
+                num_accesses++;
+
+                for(i=0;i<strlen(str);i++)
+                   if(str[i] == '\n')
+                      str[i] = '\0';
+               
+                char addr[8];
+                for(i=0;i<strlen(str);i++)
+                {
+                    if(str[1] != ' ')
+                        addr[i] = str[i];
+                    else
+                    {
+                        addr[i] = '\0';
+                        break;
+                    }
+                }
+                
+                for(i=0;i<strlen(str);i++)
+                {
+                    if(str[i] == 't')
+                        t = 1;
+                    else
+                        t = 0;
+                }
+
+                branch_addr = 0;
+                num_addr = (unsigned long long)strtoul(addr, NULL, 16);
+                num_addr = num_addr>>2;
+
+                for(i=0;i<address_bits;i++)
+                    branch_addr = branch_addr + (num_addr&(1<<i));
+
+                //XOR operation for branch address
+                branch_addr = branch_addr ^ (bhr << (address_bits - bhr_bits));    
+
+                //Mis-Prediction Count
+                if(gshare_prediction_table[branch_addr] > 1 && t == 0)
+                    branch_mispredicts++;
+                else if(gshare_prediction_table[branch_addr] < 2 && t == 1)
+                    branch_mispredicts++;
+
+ //               printf("GP: %llu\n",branch_addr);
+                //Update Prediction Table  & Branch History Register
+                if(t == 1)
+                {
+                   if(gshare_prediction_table[branch_addr] != 3)
+                       gshare_prediction_table[branch_addr]++;
+//                   printf("Old BHR: %d\n",bhr);
+                    bhr = bhr >> 1;
+                    bhr = bhr | (1<<(bhr_bits-1));
+//                   printf("New BHR: %d\n",bhr);
+                }
+                else if (t == 0)
+                {
+                   if(gshare_prediction_table[branch_addr] != 0)
+                       gshare_prediction_table[branch_addr]--;
+//                   printf("Old BHR: %d\n",bhr);
+                    bhr = bhr >> 1;
+                    bhr = bhr & (~(1<<bhr_bits));
+//                   printf("New BHR: %d\n",bhr);
+                }
+            }
+
+            //Output
+            printf("COMMAND\n");
+            printf(" ./sim gshare %d %d %s\n",address_bits,bhr_bits,argv[4]);
+            printf("OUTPUT\n");
+            printf(" number of predictions:    %llu\n",num_accesses);
+            printf(" number of mispredictions: %llu\n",branch_mispredicts);
+            printf(" misprediction rate:       %0.2f%\n",(double)100*branch_mispredicts/num_accesses);
+            printf("FINAL GSHARE CONTENTS\n");
+            
+            for(i=0;i<pow(2,address_bits);i++)
+                printf(" %d  %d\n",i,gshare_prediction_table[i]);
+            fclose(trace);
         }
         else if(!strcmp(argv[1],"hybrid"))
         {
         }
+
      
     return 0;
 }
